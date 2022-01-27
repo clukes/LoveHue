926aa1dfe7adfa478d4d1c32cee9e0cbf354e161
clukes <clukes@me.com>
2022-01-27 22:22:10 +0000

    Added timestamps to yours and partners pages.

--- lib/widgets/bar_builders.dart
+++ lib/widgets/bar_builders.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:relationship_bars/models/relationship_bar_model.dart';
 import 'package:relationship_bars/providers/application_state.dart';
@@ -20,38 +21,59 @@ Widget buildBars(
   if (!snapshot.hasData) {
     return const Center(child: CircularProgressIndicator());
   }
-  final RelationshipBarDocument latestBarDoc = RelationshipBarDocument.fromQuerySnapshot(snapshot.requireData)[0];
-  final List<RelationshipBar>? bars = latestBarDoc.barList;
-
-  return BarListBuilder(bars: bars, itemBuilderFunction: itemBuilderFunction);
+  RelationshipBarDocument? latestBarDoc;
+  List<RelationshipBarDocument> listBars = RelationshipBarDocument.fromQuerySnapshot(snapshot.requireData);
+  print(listBars);
+  if(listBars.isNotEmpty) {
+    latestBarDoc = listBars.first;
+  }
+  print(latestBarDoc);
+  return BarDocBuilder(barDoc: latestBarDoc, itemBuilderFunction: itemBuilderFunction);
 }
 
-class BarListBuilder extends StatelessWidget {
-  final List<RelationshipBar>? bars;
-  final Widget Function(BuildContext context, RelationshipBar bar) itemBuilderFunction;
+class BarDocBuilder extends StatelessWidget {
+  final RelationshipBarDocument? barDoc;
+  final Widget Function(BuildContext context, RelationshipBar bar)
+      itemBuilderFunction;
 
-  const BarListBuilder({Key? key, this.bars, required this.itemBuilderFunction})
+  const BarDocBuilder({Key? key, this.barDoc, required this.itemBuilderFunction})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    print("LIST");
-    if (bars != null) {
-      return ListView.separated(
-        padding: const EdgeInsets.fromLTRB(16, 16, 16, kFloatingActionButtonMargin + 128),
-        itemCount: bars!.length,
-        separatorBuilder: (BuildContext context, int index) => const Divider(),
-        itemBuilder: (context, index) =>
-            itemBuilderFunction(context, bars![index]),
-      );
+    print("LISTBuilder");
+    if (barDoc != null) {
+      List<RelationshipBar>? bars = barDoc!.barList ?? [];
+      return Column(children: [
+        (barDoc?.timestamp != null)
+        ? Center(
+          child: Text("Last updated at: ${formatTimestamp(barDoc!.timestamp!)}"),
+        )
+        : const SizedBox.shrink(),
+        Expanded(
+          child: ListView.separated(
+          padding: const EdgeInsets.fromLTRB(
+              16, 16, 16, kFloatingActionButtonMargin + 128),
+          itemCount: bars.length,
+          separatorBuilder: (BuildContext context, int index) =>
+              const Divider(),
+          itemBuilder: (context, index) =>
+              itemBuilderFunction(context, bars[index]),
+        ),
+        )
+      ]);
     }
     return const Center(child: Text("No Relationship Bars"));
   }
 
+  String formatTimestamp(Timestamp timestamp) {
+    return DateFormat.yMMMEd().add_jm().format(timestamp.toDate().toLocal());
+  }
 }
 
 Widget interactableBarBuilder(BuildContext context, RelationshipBar bar) {
-  WidgetsBinding.instance?.addPostFrameCallback((_) => YourBarsState.instance.barsReset=false);
+  WidgetsBinding.instance
+      ?.addPostFrameCallback((_) => YourBarsState.instance.barsReset = false);
   return InteractableBarSlider(relationshipBar: bar);
 }
 
@@ -61,10 +83,15 @@ Widget nonInteractableBarBuilder(BuildContext context, RelationshipBar bar) {
   return NonInteractableBarSlider(relationshipBar: bar);
 }
 
-Widget barStreamBuilder(String id, Widget Function(BuildContext context, RelationshipBar bar) itemBuilderFunction) {
+Widget barStreamBuilder(
+    String id,
+    Widget Function(BuildContext context, RelationshipBar bar)
+        itemBuilderFunction) {
   print("BUILDER");
   return StreamBuilder<QuerySnapshot<RelationshipBarDocument>>(
-      stream: userBarsFirestoreRef(id).orderBy(RelationshipBarDocument.columnTimestamp, descending: true).snapshots(),
+      stream: userBarsFirestoreRef(id)
+          .orderBy(RelationshipBarDocument.columnTimestamp, descending: true)
+          .snapshots(),
       builder: (context, snapshot) {
         return buildBars(context, snapshot, itemBuilderFunction);
       });
