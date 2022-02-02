2fa4ffd02190c8c519c43aba5e8d6dadad75d01f
clukes <clukes@me.com>
2022-02-02 20:09:21 +0000

    Improved timestamp formatting.

--- lib/pages/partners_bars_page.dart
+++ lib/pages/partners_bars_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:relationship_bars/providers/partners_info_state.dart';
+import 'package:relationship_bars/widgets/app_bars.dart';
 import 'package:relationship_bars/widgets/bar_builders.dart';
 import 'package:relationship_bars/widgets/link_partner_screen.dart';
 
@@ -17,25 +18,26 @@ class _PartnersBarsState extends State<PartnersBars> with AutomaticKeepAliveClie
 
   @override
   Widget build(BuildContext context) {
+    ValueListenableBuilder<String> listenableTitle = ValueListenableBuilder<String>(
+        valueListenable: PartnersInfoState.instance.partnersName,
+        builder: (BuildContext context, value, Widget? child) => Text("$value's Bars"));
+    super.build(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Consumer<PartnersInfoState>(builder: (context, partnersInfoState, _) {
-          String title = "Partner Link";
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            BarsPageAppBar(barTitleWidget: listenableTitle),
+          ];
+        },
+        body: Consumer<PartnersInfoState>(builder: (context, partnersInfoState, _) {
           if (partnersInfoState.partnerLinked) {
-            String name = partnersInfoState.partnersInfo?.displayName ?? "Partner";
-            title = "$name's Bars";
+            print("Partner: " + (partnersInfoState.partnersInfo?.partnerID ?? ''));
+            return barStreamBuilder(partnersInfoState.partnersID!, nonInteractableBarBuilder);
           }
-          return Text(title);
+          print("NOT LINKED");
+          return LinkPartnerScreen(partnersInfoState: partnersInfoState);
         }),
       ),
-      body: Consumer<PartnersInfoState>(builder: (context, partnersInfoState, _) {
-        if (partnersInfoState.partnerLinked) {
-          print("Partner: " + (partnersInfoState.partnersInfo?.partnerID ?? ''));
-          return barStreamBuilder(partnersInfoState.partnersID!, nonInteractableBarBuilder);
-        }
-        print("NOT LINKED");
-        return LinkPartnerScreen(partnersInfoState: partnersInfoState);
-      }),
     );
   }
 }
