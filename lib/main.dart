d08b1f45f02bb65333c29e169d23f572fae76842
clukes <clukes@me.com>
2022-03-03 18:56:02 +0000

    Code cleanup and commenting complete.

--- lib/main.dart
+++ lib/main.dart
@@ -35,63 +35,73 @@ TODO: Have cancel/back button on magic link screens.
 TODO: Optimization, only update widgets that need updating.
  */
 
+/// Entry point with initializers.
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await ApplicationState.instance.init();
+
+  // Add licenses for the fonts.
   LicenseRegistry.addLicense(() async* {
     String license = await rootBundle.loadString('assets/google_fonts/DMSansOFL.txt');
     yield LicenseEntryWithLineBreaks(['google_fonts'], license);
     license = await rootBundle.loadString('assets/google_fonts/PoppinsOFL.txt');
     yield LicenseEntryWithLineBreaks(['google_fonts'], license);
   });
-  // Only use the included google fonts, don't fetch from online
+  // Only uses the bundled google fonts, prevents fetching from online.
   GoogleFonts.config.allowRuntimeFetching = false;
   runApp(const RelationshipBarsApp());
 }
 
+/// Initial widget for the app.
 class RelationshipBarsApp extends StatelessWidget {
   const RelationshipBarsApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-        providers: [
-          ChangeNotifierProvider<ApplicationState>.value(value: ApplicationState.instance),
-          ChangeNotifierProvider<UserInfoState>.value(value: UserInfoState.instance),
-          ChangeNotifierProvider<PartnersInfoState>.value(value: PartnersInfoState.instance),
-          ChangeNotifierProvider<YourBarsState>.value(value: YourBarsState.instance),
-        ],
-        child: MaterialApp(
-            debugShowCheckedModeBanner: false,
-            showPerformanceOverlay: false,
-            title: 'Relationship Bars',
-            theme: lightThemeData,
-            home: AnnotatedRegion<SystemUiOverlayStyle>(
-              value: SystemUiOverlayStyle.dark.copyWith(statusBarIconBrightness: Brightness.light),
-              child: SafeArea(
-                child: StreamBuilder(
-                    stream: FirebaseAuth.instance.authStateChanges(),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.active) {
-                        if (snapshot.hasData) {
-                          return responsiveLayout;
-                        } else if (snapshot.hasError) {
-                          return Center(
-                            child: Text('Error: ${snapshot.error}'),
-                          );
-                        }
-                      }
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                      return const SignInPage();
-                    }),
-              ),
-            )));
+      // Setup providers for states.
+      providers: [
+        ChangeNotifierProvider<ApplicationState>.value(value: ApplicationState.instance),
+        ChangeNotifierProvider<UserInfoState>.value(value: UserInfoState.instance),
+        ChangeNotifierProvider<PartnersInfoState>.value(value: PartnersInfoState.instance),
+        ChangeNotifierProvider<YourBarsState>.value(value: YourBarsState.instance),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        showPerformanceOverlay: false,
+        title: 'Relationship Bars', //TODO: Set to correct name.
+        // Currently there is only one theme, a light one.
+        theme: lightThemeData,
+        home: AnnotatedRegion<SystemUiOverlayStyle>(
+          // Ensures that the status bar stays dark with light text.
+          value: SystemUiOverlayStyle.dark.copyWith(statusBarIconBrightness: Brightness.light),
+          child: SafeArea(
+            child: StreamBuilder(
+                stream: FirebaseAuth.instance.authStateChanges(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.active) {
+                    if (snapshot.hasData) {
+                      return responsiveLayout;
+                    } else if (snapshot.hasError) {
+                      return Center(
+                        child: Text('Error: ${snapshot.error}'),
+                      );
+                    }
+                  }
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  return const SignInPage();
+                },
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
