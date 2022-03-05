71208c6a63919db6b226759e4fcd3ca914f0f187
clukes <clukes@me.com>
2022-03-05 00:13:27 +0000

    Logo added

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -3,44 +3,11 @@ const webScreenSize = 900;
 
 /// Default bar labels for new user.
 const List<String> defaultBarLabels = [
-  // TODO: Improve these
   "Verbal Affection",
   "Physical Affection",
   "Undivided Attention",
   "Shared Activities",
-
-  "Giving Gifts",
-  "Acts of Service",
-  "Physical Touch",
+  "Communication",
+  "Responsibilities",
+  "Gifts/Surprises/Planning things to do",
 ];
-
-// Extra: Communication, Expression of Affection and Love
-// https://medium.com/hello-love/the-7-languages-of-love-expressed-by-truly-in-love-couples-d2f745565c3d
-
-// 7 Love styles
-// ACTIVITY
-// APPRECIATION
-// PHYSICAL
-// EMOTIONAL
-// INTELLECTUAL
-// PRACTICAL
-// FINANCIAL
-// https://www.truity.com/test-results/lovelanguage/18116/28369376
-
-// 7D: Commitment, Intimacy, Autonomy, Communication, Passion, Teamwork, Growth & spirituality
-// https://www.thecouplescenter.org/7d-of-relationship/
-
-
-// Communication: Conflict resolution, Openness/honesty
-
-// Communication;
-// up-close &
-// personal
-// Communication;
-// problem
-// solving
-// Finances & Money matters
-// Home chores & organisation
-// Family matters
-// Time apart & activities
-// Time together & activities
