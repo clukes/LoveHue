96d3e5b5d7d2d51e7b5de04791abfb2ca4021e45
clukes <clukes@me.com>
2022-07-02 18:12:50 +0100

    Add dotenv

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,8 +1,11 @@
-import '.firebase/firebase_options_dev.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'firebase/firebase_options_dev.dart';
 import 'main_common.dart';
 import 'utils/app_info_class.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: ".dev.env");
   mainCommon(
     DefaultFirebaseOptions.currentPlatform,
     const AppInfo(
