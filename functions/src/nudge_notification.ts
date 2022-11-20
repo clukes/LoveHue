8fe73ab9445ec54ea64a03f7d7e988cbfc0ac3fb
clukes <clukes@me.com>
2022-11-20 12:13:08 +0000

    Add tests for cloud functions

--- functions/src/nudge_notification.ts
+++ functions/src/nudge_notification.ts
@@ -1,8 +1,9 @@
 "use strict";
 
-import * as functions from "firebase-functions";
+import { firestore, logger } from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as config from "../../assets/configs/notification_configs.json";
+
 admin.initializeApp();
 
 const notificationDocPath = config.notificationCollectionPath + "/{userId}";
@@ -16,78 +17,83 @@ const minimumMillisecondsBetweenNudges =
  *
  * User saves a requested timestamp to `/nudge/{userId}`.
  */
-export const sendNudgeNotification = functions.firestore
+export const sendNudgeNotification = firestore
     .document(notificationDocPath)
     .onWrite(async (change, context) => {
-      const userId: string = context.params.userId;
+        const userId: string = context.params.userId;
 
-      // If the request timestamp hasn't been updated, we exit.
-      if (
-        change.before.get(requestTimestampFieldName) ==
+        // If the request timestamp hasn't been updated, we exit.
+        if (
+            change.before.get(requestTimestampFieldName) ==
             change.after.get(requestTimestampFieldName)
-      ) {
-        return functions.logger.debug(
-            "User",
-            userId,
-            "had no change of request timestamp in notification doc, at:",
-            notificationDocPath
+        ) {
+            logger.debug(
+                "User",
+                userId,
+                "had no change of request timestamp in notification doc, at:",
+                notificationDocPath
+            );
+            return false;
+        }
+
+        // If the document is deleted, we exit the function.
+        if (!change.after.exists) {
+            logger.debug(
+                "User",
+                userId,
+                "has had notification doc deleted, at:",
+                notificationDocPath
+            );
+            return false;
+        }
+
+        logger.log("User request nudge notification:", userId);
+
+        // Check if it's been long enough since last request.
+        const requestedTimestamp = getTimestampFromDoc(
+            change.after,
+            requestTimestampFieldName
         );
-      }
 
-      // If the document is deleted, we exit the function.
-      if (!change.after.exists) {
-        return functions.logger.debug(
-            "User",
+        if (requestedTimestamp == null) return false;
+
+        if (!isRequestValid(change.after, userId, requestedTimestamp))
+            return false;
+
+        // Set notification details.
+        const payload = {
+            notification: {
+                title: "Your partner wants you to update!",
+                body: "Your partner sent you a nudge to update your LoveHue bars.",
+            },
+        };
+
+        // Send notification to topic, using the users userId.
+        const topic = userId;
+        const response = await admin.messaging().sendToTopic(topic, payload);
+        logger.log(
+            "Notification request by user:",
             userId,
-            "has had notification doc deleted, at:",
-            notificationDocPath
+            "sent to topic:",
+            topic,
+            "with response:",
+            response,
+            "and with payload:",
+            payload
         );
-      }
 
-      functions.logger.log("User request nudge notification:", userId);
+        // Update completed timestamp
+        await change.after.ref.update({
+            [completeTimestampFieldName]: requestedTimestamp.toString(),
+        });
 
-      // Check if it's been long enough since last request.
-      const requestedTimestamp = getTimestampFromDoc(
-          change.after,
-          requestTimestampFieldName
-      );
-      if (requestedTimestamp == null) return;
-
-      if (!isRequestValid(change.after, userId, requestedTimestamp)) return;
-
-      // Set notification details.
-      const payload = {
-        notification: {
-          title: "Your partner wants you to update!",
-          body: "Your partner sent you a nudge to update your LoveHue bars.",
-        },
-      };
-
-      // Send notification to topic, using the users userId.
-      const topic = userId;
-      const response = await admin.messaging().sendToTopic(topic, payload);
-      functions.logger.log(
-          "Notification request by user:",
-          userId,
-          "sent to topic:",
-          topic,
-          "with response:",
-          response,
-          "and with payload:",
-          payload
-      );
-
-      // Update completed timestamp
-      await change.after.ref.update({
-        completeTimestampFieldName: requestedTimestamp,
-      });
-
-      return functions.logger.debug(
-          "Completed timestamp updated for:",
-          userId,
-          "with timestamp:",
-          requestedTimestamp
-      );
+        logger.debug(
+            "Completed timestamp updated for:",
+            userId,
+            "with timestamp:",
+            requestedTimestamp
+        );
+        return true;
     });
 
 /**
@@ -103,27 +109,27 @@ function getTimestampFromDoc(
     doc: admin.firestore.DocumentSnapshot,
     fieldName: string
 ): number | undefined {
-  const timestampString = doc.get(fieldName);
-  const timestamp: number = parseInt(timestampString);
-  if (isNaN(timestamp) || timestamp == undefined) {
-    functions.logger.error(
-        fieldName,
-        "for doc id:",
-        doc.id,
-        "gave an invalid number:",
-        timestampString
-    );
-    return;
-  }
+    const timestampString = doc.get(fieldName);
+    const timestamp: number = parseInt(timestampString);
+    if (isNaN(timestamp) || timestamp == undefined) {
+        logger.error(
+            fieldName,
+            "for doc id:",
+            doc.id,
+            "gave an invalid number:",
+            timestampString
+        );
+        return;
+    }
 
-  return timestamp;
+    return timestamp;
 }
 
 /**
  * If the user has completed a nudge in the past, check that it's been at least
  * `minimumMillisecondsBetweenNudges` milliseconds since the last nudge
  * @param {admin.firestore.DocumentSnapshot} doc
- *  - this is the document that we're checking to see if
+ *  - this is the document that we're checking
  * @param {string} userId - The user ID of the user who is requesting a nudge.
  * @param {number} requestedTimestamp
  *  - The timestamp of the request to send a nudge.
@@ -136,21 +142,37 @@ function isRequestValid(
     userId: string,
     requestedTimestamp: number
 ): boolean {
-  if (doc.get(completeTimestampFieldName) == null) {
-    functions.logger.debug("No completed timestamp user:", userId);
-    return true;
-  }
+    if (doc.get(completeTimestampFieldName) == null) {
+        logger.debug("No completed timestamp user:", userId);
+        return true;
+    }
 
-  const lastCompletedTimestamp = getTimestampFromDoc(
-      doc,
-      completeTimestampFieldName
-  );
-  if (lastCompletedTimestamp == null) return false;
+    const lastCompletedTimestamp = getTimestampFromDoc(
+        doc,
+        completeTimestampFieldName
+    );
+    if (lastCompletedTimestamp == null) {
+        logger.warn("No lastCompletedTimestamp found for user:", userId);
+        return false;
+    }
 
-  const millisecondsSinceNudge = requestedTimestamp - lastCompletedTimestamp;
-  if (millisecondsSinceNudge < minimumMillisecondsBetweenNudges) {
-    functions.logger.warn(
-        "Hasn't been long enough since last nudge for user:",
+    const millisecondsSinceNudge = requestedTimestamp - lastCompletedTimestamp;
+    if (millisecondsSinceNudge < minimumMillisecondsBetweenNudges) {
+        logger.warn(
+            "Hasn't been long enough since last nudge for user:",
+            userId,
+            "last timestamp was:",
+            lastCompletedTimestamp,
+            "requestedTimestamp was:",
+            requestedTimestamp,
+            "difference in milliseconds was:",
+            millisecondsSinceNudge
+        );
+        return false;
+    }
+
+    logger.debug(
+        "Has been long enough since last nudge for user:",
         userId,
         "last timestamp was:",
         lastCompletedTimestamp,
@@ -159,19 +181,6 @@ function isRequestValid(
         "difference in milliseconds was:",
         millisecondsSinceNudge
     );
-    return false;
-  }
 
-  functions.logger.debug(
-      "Has been long enough since last nudge for user:",
-      userId,
-      "last timestamp was:",
-      lastCompletedTimestamp,
-      "requestedTimestamp was:",
-      requestedTimestamp,
-      "difference in milliseconds was:",
-      millisecondsSinceNudge
-  );
-
-  return true;
+    return true;
 }
---
+++ functions/test/nudge_notification.test.ts
@@ -0,0 +1,363 @@
+"use strict";
+
+import { assert } from "chai";
+import sinon = require("sinon");
+import admin = require("firebase-admin");
+import { config as firebaseConfig } from "firebase-functions";
+import * as config from "../../assets/configs/notification_configs.json";
+
+import {
+    MessagingPayload,
+    MessagingOptions,
+    MessagingTopicResponse,
+} from "firebase-admin/lib/messaging/messaging-api";
+
+const tester = require("firebase-functions-test")();
+
+const userId = "1";
+const context = { params: { userId: userId } };
+
+const requestTimestampFieldName = config.columnRequested;
+const completeTimestampFieldName = config.columnCompleted;
+const minimumMillisecondsBetweenNudges =
+    config.minimumMillisecondsBetweenNudges;
+
+describe("Cloud Functions", () => {
+    let myFunctions: { sendNudgeNotification: any },
+        adminInitStub: sinon.SinonStub<
+            [options?: admin.AppOptions | undefined, name?: string | undefined],
+            admin.app.App
+        >,
+        messagingTopicStub: sinon.SinonStub<
+            [
+                topic: string,
+                payload: MessagingPayload,
+                options?: MessagingOptions | undefined
+            ],
+            Promise<MessagingTopicResponse>
+        >;
+
+    admin.initializeApp(firebaseConfig().firebase);
+
+    before(() => {
+        // [START stubAdminInit]
+        // If index.js calls admin.initializeApp at the top of the file,
+        // we need to stub it out before requiring index.js. This is because the
+        // functions will be executed as a part of the require process.
+        // Here we stub admin.initializeApp to be a dummy function that doesn't do anything.
+        tester.firestore.exampleDocumentSnapshot();
+        adminInitStub = sinon.stub(admin, "initializeApp");
+        // Now we can require index.js and save the exports inside a namespace called myFunctions.
+        myFunctions = require("../src/index");
+        // [END stubAdminInit]
+    });
+
+    after(() => {
+        adminInitStub.restore();
+
+        tester.cleanup();
+    });
+
+    beforeEach(() => {
+        messagingTopicStub = sinon.stub(admin.messaging(), "sendToTopic");
+    });
+
+    afterEach(() => {
+        messagingTopicStub.restore();
+    });
+
+    describe("sendNudgeNotification", () => {
+        it("is invoked on document write", async () => {
+            const { wrapped, snapshotChange, updateStub } = setUpDocumentUpdate(
+                myFunctions,
+                {},
+                {}
+            );
+
+            return await assertNotificationNotSent(
+                wrapped,
+                snapshotChange,
+                messagingTopicStub,
+                updateStub
+            );
+        });
+
+        it("doesn't send notification if no change in timestamp", async () => {
+            const beforeData = {
+                [requestTimestampFieldName]: "1",
+            };
+
+            const { wrapped, snapshotChange, updateStub } = setUpDocumentUpdate(
+                myFunctions,
+                beforeData,
+                beforeData
+            );
+
+            return await assertNotificationNotSent(
+                wrapped,
+                snapshotChange,
+                messagingTopicStub,
+                updateStub
+            );
+        });
+
+        it("doesn't send notification if document is deleted", async () => {
+            const { wrapped, snapshotChange, updateStub } = setUpDocumentUpdate(
+                myFunctions,
+                {
+                    [requestTimestampFieldName]: "1",
+                },
+                {}
+            );
+
+            return await assertNotificationNotSent(
+                wrapped,
+                snapshotChange,
+                messagingTopicStub,
+                updateStub
+            );
+        });
+
+        it("doesn't send notification if after requested timestamp is NaN", async () => {
+            const { wrapped, snapshotChange, updateStub } = setUpDocumentUpdate(
+                myFunctions,
+                {},
+                { [requestTimestampFieldName]: "not a number" }
+            );
+
+            return await assertNotificationNotSent(
+                wrapped,
+                snapshotChange,
+                messagingTopicStub,
+                updateStub
+            );
+        });
+
+        it("doesn't send notification if after requested timestamp is empty", async () => {
+            const { wrapped, snapshotChange, updateStub } = setUpDocumentUpdate(
+                myFunctions,
+                {},
+                { [requestTimestampFieldName]: "" }
+            );
+
+            return await assertNotificationNotSent(
+                wrapped,
+                snapshotChange,
+                messagingTopicStub,
+                updateStub
+            );
+        });
+
+        it("doesn't send notification if after completed timestamp is NaN", async () => {
+            const { wrapped, snapshotChange, updateStub } = setUpDocumentUpdate(
+                myFunctions,
+                {},
+                {
+                    [completeTimestampFieldName]: "not a number",
+                    [requestTimestampFieldName]: "1",
+                }
+            );
+
+            return await assertNotificationNotSent(
+                wrapped,
+                snapshotChange,
+                messagingTopicStub,
+                updateStub
+            );
+        });
+
+        it("doesn't send notification if after completed timestamp is empty", async () => {
+            const { wrapped, snapshotChange, updateStub } = setUpDocumentUpdate(
+                myFunctions,
+                {},
+                {
+                    [completeTimestampFieldName]: "",
+                    [requestTimestampFieldName]: "1",
+                }
+            );
+
+            return await assertNotificationNotSent(
+                wrapped,
+                snapshotChange,
+                messagingTopicStub,
+                updateStub
+            );
+        });
+
+        it("doesn't send notification if difference between requested and completed timestamp < minimumMillisecondsBetweenNudges", async () => {
+            const { wrapped, snapshotChange, updateStub } = setUpDocumentUpdate(
+                myFunctions,
+                {},
+                {
+                    [completeTimestampFieldName]: "1",
+                    [requestTimestampFieldName]: "2",
+                }
+            );
+
+            return await assertNotificationNotSent(
+                wrapped,
+                snapshotChange,
+                messagingTopicStub,
+                updateStub
+            );
+        });
+
+        it("sends notification if after completed timestamp is null", async () => {
+            const { wrapped, snapshotChange, updateStub } = setUpDocumentUpdate(
+                myFunctions,
+                {},
+                {
+                    [completeTimestampFieldName]: null,
+                    [requestTimestampFieldName]: "1",
+                }
+            );
+
+            return await assertNotificationSent(
+                wrapped,
+                snapshotChange,
+                messagingTopicStub,
+                updateStub
+            );
+        });
+
+        it("sends notification if difference between requested and completed timestamp == minimumMillisecondsBetweenNudges", async () => {
+            const { wrapped, snapshotChange, updateStub } = setUpDocumentUpdate(
+                myFunctions,
+                {},
+                {
+                    [completeTimestampFieldName]: `0`,
+                    [requestTimestampFieldName]: `${minimumMillisecondsBetweenNudges}`,
+                }
+            );
+
+            return await assertNotificationSent(
+                wrapped,
+                snapshotChange,
+                messagingTopicStub,
+                updateStub
+            );
+        });
+
+        it("sends notification if difference between requested and completed timestamp > minimumMillisecondsBetweenNudges", async () => {
+            const { wrapped, snapshotChange, updateStub } = setUpDocumentUpdate(
+                myFunctions,
+                {},
+                {
+                    [completeTimestampFieldName]: "0",
+                    [requestTimestampFieldName]: `${
+                        minimumMillisecondsBetweenNudges + 1
+                    }`,
+                }
+            );
+
+            return await assertNotificationSent(
+                wrapped,
+                snapshotChange,
+                messagingTopicStub,
+                updateStub
+            );
+        });
+
+        it("sends notification to correct topic with correct payload", async () => {
+            const { wrapped, snapshotChange, updateStub } = setUpDocumentUpdate(
+                myFunctions,
+                {},
+                {
+                    [completeTimestampFieldName]: "0",
+                    [requestTimestampFieldName]: `${minimumMillisecondsBetweenNudges}`,
+                }
+            );
+
+            const expectedPayload = {
+                notification: {
+                    title: "Your partner wants you to update!",
+                    body: "Your partner sent you a nudge to update your LoveHue bars.",
+                },
+            };
+
+            assert.equal(await wrapped(snapshotChange, context), true);
+            assert.equal(
+                messagingTopicStub.calledOnceWithExactly(
+                    userId,
+                    expectedPayload
+                ),
+                true
+            );
+            return assert.equal(updateStub.calledOnce, true);
+        });
+
+        it("sends notification and updates completed timestamp after sending", async () => {
+            const requestedTimestamp = `${minimumMillisecondsBetweenNudges}`;
+            const { wrapped, snapshotChange, updateStub } = setUpDocumentUpdate(
+                myFunctions,
+                {},
+                {
+                    [completeTimestampFieldName]: "0",
+                    [requestTimestampFieldName]: requestedTimestamp,
+                }
+            );
+
+            assert.equal(await wrapped(snapshotChange, context), true);
+            assert.equal(messagingTopicStub.calledOnce, true);
+
+            return assert.equal(
+                updateStub.calledOnceWithExactly({
+                    [completeTimestampFieldName]: requestedTimestamp,
+                }),
+                true
+            );
+        });
+    });
+});
+
+function setUpDocumentUpdate(
+    myFunctions: { sendNudgeNotification: any },
+    beforeData: { [x: string]: string },
+    afterData: { [x: string]: string | null }
+) {
+    const snapshotBefore = tester.firestore.makeDocumentSnapshot(beforeData);
+    const snapshotAfter = tester.firestore.makeDocumentSnapshot(afterData);
+    const snapshotChange = tester.makeChange(snapshotBefore, snapshotAfter);
+    const updateStub = sinon.stub();
+    const wrapped = tester.wrap(myFunctions.sendNudgeNotification);
+
+    snapshotChange.after.ref.update = updateStub;
+    updateStub.withArgs(snapshotChange.after.data()).returns(true);
+    return { wrapped, snapshotChange, updateStub };
+}
+
+async function assertNotificationNotSent(
+    wrapped: (arg0: any, arg1: { params: { userId: string } }) => any,
+    snapshotChange: any,
+    messagingTopicStub: sinon.SinonStub<
+        [
+            topic: string,
+            payload: MessagingPayload,
+            options?: MessagingOptions | undefined
+        ],
+        Promise<MessagingTopicResponse>
+    >,
+    updateStub: sinon.SinonStub<any[], any>
+) {
+    assert.equal(await wrapped(snapshotChange, context), false);
+    assert.equal(messagingTopicStub.called, false);
+    return assert.equal(updateStub.calledOnce, false);
+}
+
+async function assertNotificationSent(
+    wrapped: (arg0: any, arg1: { params: { userId: string } }) => any,
+    snapshotChange: any,
+    messagingTopicStub: sinon.SinonStub<
+        [
+            topic: string,
+            payload: MessagingPayload,
+            options?: MessagingOptions | undefined
+        ],
+        Promise<MessagingTopicResponse>
+    >,
+    updateStub: sinon.SinonStub<any[], any>
+) {
+    assert.equal(await wrapped(snapshotChange, context), true);
+    assert.equal(messagingTopicStub.called, true);
+    return assert.equal(updateStub.calledOnce, true);
+}
