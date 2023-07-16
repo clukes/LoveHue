40a1812eac209497fa409c97dbfea6b4ba3ada45
clukes <clukes@me.com>
2023-07-16 14:45:26 +0100

    Add test logging

--- integration_test/e2e_tests.dart
+++ integration_test/e2e_tests.dart
@@ -14,9 +14,13 @@ void main() {
 
   group('end-to-end test', () {
     testWidgets('test sign in and updating bars', (WidgetTester tester) async {
+      debugPrint("Starting app...");
       await startUpApp(tester);
+      debugPrint("Testing anonymous sign in...");
       await testAnonymousSignIn(tester, binding);
+      debugPrint("Testing updating bars...");
       await testUpdatingBars(tester, binding);
+      debugPrint("Tests completed.");
     });
   });
 }
@@ -46,11 +50,11 @@ Future startUpApp(WidgetTester tester) async {
 
 Future testAnonymousSignIn(
     WidgetTester tester, IntegrationTestWidgetsFlutterBinding binding) async {
-  expect(find.text('Sign in'), findsOneWidget);
+  expect(find.text('Sign in'), findsOneWidget, reason: "No sign in button");
 
   // Find and tap the "Skip login" button.
   final skipLoginButton = find.text('Skip Login');
-  expect(skipLoginButton, findsOneWidget);
+  expect(skipLoginButton, findsOneWidget, reason: "No skip login button");
 
   await tester.tap(skipLoginButton);
 
@@ -59,12 +63,12 @@ Future testAnonymousSignIn(
 
   // Verify that an anonymous user is created and signed in.
   final currentUser = FirebaseAuth.instance.currentUser;
-  expect(currentUser, isNotNull);
-  expect(currentUser!.isAnonymous, isTrue);
+  expect(currentUser, isNotNull, reason: "User not signed in");
+  expect(currentUser!.isAnonymous, isTrue, reason: "User was not anonymous");
 
   // Verify that "Your bars" is displayed on the screen.
   final yourBarsText = find.text('Your Bars');
-  expect(yourBarsText, findsOneWidget);
+  expect(yourBarsText, findsOneWidget, reason: "Your Bars title not found");
   await takeScreenshot(tester, binding, "SkipLogin-LoggedIn");
 }
 
@@ -72,7 +76,7 @@ Future testUpdatingBars(
     WidgetTester tester, IntegrationTestWidgetsFlutterBinding binding) async {
   // Get current user
   final user = FirebaseAuth.instance.currentUser;
-  expect(user, isNotNull);
+  expect(user, isNotNull, reason: "No user signed in");
   final userId = user!.uid;
 
   // Get initial saved bars count
@@ -84,17 +88,19 @@ Future testUpdatingBars(
       await userBarsCollection.count().get().then((snapshot) => snapshot.count);
 
   // Find a bar and update it.
-  expect(find.text('Your Bars'), findsOneWidget);
+  expect(find.text('Your Bars'), findsOneWidget,
+      reason: "Your bars title not found");
   final bars = find.byType(InteractableBarSlider);
-  expect(bars, findsAtLeastNWidgets(1));
-  final barToUpdate = find.descendant(of: bars.first, matching: find.byType(Slider));
+  expect(bars, findsAtLeastNWidgets(1), reason: "No user bars found");
+  final barToUpdate =
+      find.descendant(of: bars.first, matching: find.byType(Slider));
   await tester.drag(barToUpdate, const Offset(20, 0));
 
   await tester.pumpAndSettle();
 
   // Find save button and tap it.
   final saveButton = find.byTooltip('Save');
-  expect(saveButton, findsOneWidget);
+  expect(saveButton, findsOneWidget, reason: "Save button not found");
   await tester.tap(saveButton);
 
   // Wait for Firebase to complete its asynchronous operations.
@@ -103,5 +109,6 @@ Future testUpdatingBars(
   // Verify that updated bars are saved in database.
   final updatedBarsCount =
       await userBarsCollection.count().get().then((snapshot) => snapshot.count);
-  expect(updatedBarsCount, greaterThan(originalBarsCount));
+  expect(updatedBarsCount, greaterThan(originalBarsCount),
+      reason: "Count of bars docs did not increase");
 }
